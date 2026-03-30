0b85283127a86e4e8b168457c919c0febce8f37d
johnmorgado81 <johnmorgado81@gmail.com>
2026-03-30 09:34:59 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 
 export const CONFIG = {
   SUPABASE_URL:      'https://gcytixguuxluijriosrm.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_KGHjMAvwBfM0Aq_Xzxj99g_PMP46VcT',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjeXRpeGd1dXhsdWlqcmlvc3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2NzgyMDgsImV4cCI6MjA5MDI1NDIwOH0.Jb81RCpCE-dYB2b-H6Pw22gxXtggBqhIBjoqVmcSMf8',
 
   APP_NAME: 'MEPC Commander',
   VERSION:  '1.1.0',
